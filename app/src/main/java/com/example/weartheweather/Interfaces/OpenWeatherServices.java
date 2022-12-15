ff3ceb5660782e149bfa615f344662c97a90d474
allard.kazuki <kazutor73@gmail.com>
2022-12-15 15:07:54 +0100

    final commit sprint1

--- app/src/main/java/com/example/weartheweather/Interfaces/OpenWeatherServices.java
+++ app/src/main/java/com/example/weartheweather/Interfaces/OpenWeatherServices.java
@@ -1,15 +1,18 @@
 package com.example.weartheweather.Interfaces;
 
 import com.example.weartheweather.Models.Forecast;
+import com.example.weartheweather.Models.Main;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface OpenWeatherServices {
 
-    @GET("forecast?lat=45.89&lon=6.12&appid=e075a0b59517e88cc46940bb262add13&units=metric")
+    @GET("forecast?lat=45.89&lon=6.12&appid=e075a0b59517e88cc46940bb262add13")
     Call<Forecast> getForecasts();
 
+    @GET("forecast?lat=45.89&lon=6.12&appid=e075a0b59517e88cc46940bb262add13")
+    Call<Main> getMain();
 
 
 }
